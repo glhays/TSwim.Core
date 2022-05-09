3669c179da3e05d2d63385eff8ad5ce62bdf67f5
Greg Hays <glhays@pacbell.net>
2022-05-09 16:33:05 -0700

    INFRA: Initialize Unit Test Project

---
+++ TSwim.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using Xunit;
+using FluentAssertions;
+
+namespace TSwim.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            true.Should().BeTrue();
+    }
+}
