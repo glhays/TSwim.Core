79ca17cc442dbff2adcb906181c93e12ccadb011
Greg Hays <glhays@pacbell.net>
2022-05-09 17:59:08 -0700

    INFRA: Intialize Acceptance Tests Project

---
+++ TSwim.Core.Tests.Acceptance/Brokers/ApiTestCollection.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TSwim.Core.Tests.Acceptance.Brokers
+{
+    [CollectionDefinition(nameof(ApiTestCollection))]
+    public class ApiTestCollection : ICollectionFixture<ApiBroker>{ }
+}
