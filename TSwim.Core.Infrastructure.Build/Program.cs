4d9489145d0d20dfb598fdac1d4ad612a7793521
Greg Hays <glhays.dev@gmail.com>
2022-05-12 17:39:50 -0700

    INFRA: Initialize Cloud Broker

--- TSwim.Core.Infrastructure.Build/Program.cs
+++ TSwim.Core.Infrastructure.Build/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
 using ADotNet.Clients;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
---
+++ TSwim.Core.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSwim.Core.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {  }
+}
