c5242f89068345be66aa1ed97abcf2cf8c7d5f0d
Greg Hays <glhays.dev@gmail.com>
2022-05-15 14:41:01 -0700

    BROKERS: Implement Cloud Management Configuration

---
+++ TSwim.Core.Infrastructure.Provision/Models/Configurations/CloudAction.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace TSwim.Core.Infrastructure.Provision.Models.Configurations
+{
+    public class CloudAction
+    {
+        public List<string> Environments { get; set; }
+    }
+}
---
+++ TSwim.Core.Infrastructure.Provision/Models/Configurations/CloudManagementConfiguration.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+namespace TSwim.Core.Infrastructure.Provision.Models.Configurations
+{
+    public class CloudManagementConfiguration
+    {
+        public string ProjectName { get; set; }
+        public CloudAction Up { get; set; }
+        public CloudAction Down { get; set; }
+    }
+}
