65d1ce26466f972d88172a7c4608b5db8b2ad7b9
Greg Hays <glhays.dev@gmail.com>
2022-05-15 15:29:48 -0700

    BROKERS: Implement Logging Broker

---
+++ TSwim.Core.Infrastructure.Provision/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+namespace TSwim.Core.Infrastructure.Provision.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogActivity(string message);
+    }
+}
