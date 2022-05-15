efb5eeb926b5eaa72ed91696eae7546e37bbc194
Greg Hays <glhays.dev@gmail.com>
2022-05-15 13:06:24 -0700

    BROKERS: Implement SQL Server

---
+++ TSwim.Core.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Storages.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Sql.Fluent;
+
+namespace TSwim.Core.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<ISqlServer> CreateSqlServerAsync(
+            string sqlServerName,
+            IResourceGroup resourceGroup)
+        {
+            return await this.azure.SqlServers
+                .Define(sqlServerName)
+                .WithRegion(Region.USWest)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithAdministratorLogin(this.adminName)
+                .WithAdministratorPassword(this.adminAccess)
+                .CreateAsync();
+        }
+    }
+}
---
+++ TSwim.Core.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Storages.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.Sql.Fluent;
+
+namespace TSwim.Core.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<ISqlServer> CreateSqlServerAsync(
+            string sqlServerName,
+            IResourceGroup resourceGroup);
+    }
+}
