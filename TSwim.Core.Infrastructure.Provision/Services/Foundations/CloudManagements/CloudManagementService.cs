44b61ca37fe31e2932145c27815fc4c66499a418
Greg Hays <glhays.dev@gmail.com>
2022-05-15 16:27:46 -0700

    FOUNDATIONS: Provision Resource Group

---
+++ TSwim.Core.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using TSwim.Core.Infrastructure.Provision.Brokers.Clouds;
+using TSwim.Core.Infrastructure.Provision.Brokers.Loggings;
+
+namespace TSwim.Core.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService()
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public async ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            string resourceGroupName =
+                $"{projectName}-RESOURCES-{environment}".ToUpper();
+
+            this.loggingBroker.LogActivity(
+                message: $"Provisioning {resourceGroupName} ...");
+
+            IResourceGroup resourceGroup = await this.cloudBroker.CreateResourceGroupAsync(
+                resourceGroupName);
+
+            this.loggingBroker.LogActivity(
+                $"Provisioning {resourceGroupName} Completed.");
+
+            return resourceGroup;
+        }
+    }
+}
---
+++ TSwim.Core.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace TSwim.Core.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public interface ICloudManagementService
+    {
+        ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment);
+    }
+}
