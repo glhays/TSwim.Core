fef1a84648bc65d80f375ad78618717be99b4aff
Greg Hays <glhays.dev@gmail.com>
2022-05-15 13:58:20 -0700

    BROKERS: Implement Web App

---
+++ TSwim.Core.Infrastructure.Provision/Brokers/Clouds/CloudBroker.WebApps.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.AppService.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace TSwim.Core.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<IWebApp> CreateWebAppAsync(
+            string webAppName,
+            string connectionString,
+            IAppServicePlan appServicePlan,
+            IResourceGroup resourceGroup)
+        {
+            return await this.azure.WebApps
+                .Define(webAppName)
+                .WithExistingWindowsPlan(appServicePlan)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithNetFrameworkVersion(NetFrameworkVersion.Parse("v.7.0"))
+                .WithConnectionString(
+                    name: "DefaultConnection",
+                    value: connectionString,
+                    type: ConnectionStringType.SQLServer)
+                .CreateAsync();               
+        }
+
+    }
+}
---
+++ TSwim.Core.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.WebApps.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace TSwim.Core.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IWebApp> CreateWebAppAsync(
+            string webAppName,
+            string connectionString,
+            IAppServicePlan appServicePlan,
+            IResourceGroup resourceGroup);
+    }
+}
