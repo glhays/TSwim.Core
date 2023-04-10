4304f6e9b55689a7017818130926624d04d958ac
Greg Hays <glhays.dev@gmail.com>
2023-04-10 16:50:11 -0700

    INFRA: Cleanup Start Files

--- TSwim.Core.API/Startup.cs
+++ TSwim.Core.API/Startup.cs
@@ -16,16 +16,15 @@ namespace TSwim.Core.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration) =>        
             Configuration = configuration;
-        }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TSwim.Core.API", Version = "v1" });
