1c4b804f244fc8568c2769d6678f4d278cc91583
Greg Hays <glhays.dev@gmail.com>
2023-05-29 13:30:19 -0700

    INFRA: Initialize New Acceptance Test Project

---
+++ TSwim.Core.Api.Tests.Acceptance/Brokers/ApiTestCollection.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace TSwim.Core.Api.Tests.Acceptance.Brokers
+{
+    [CollectionDefinition(nameof(ApiTestCollection))]
+    public class ApiTestCollection : ICollectionFixture<TSwimCoreApiBroker>
+    {
+    }
+}
---
+++ TSwim.Core.Api.Tests.Acceptance/Brokers/TSwimCoreApiBroker.Home.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace TSwim.Core.Api.Tests.Acceptance.Brokers
+{
+    public partial class TSwimCoreApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+
+        public async ValueTask<string> GetHomeMessage() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
