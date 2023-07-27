c9cb92434fdf2f27959f620e2e0754e924f87ce4
Greg Hays <glhays.dev@gmail.com>
2023-07-27 14:30:13 -0700

    CODE RUB: Data Company Add
    Closes #8

---
+++ TSwim.Core/Brokers/Storages/StorageBroker.Companies.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------------
+// Participation to the Coalition of the Good-Hearted Engineers WorldWide
+// Copyright © TSwim and contributors. All rights reserved.
+// GLHays ©2022 Founder,Developer - (TS)Warehouse Inventory Management
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+// -----------------------------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using TSwim.Core.Models.Companies;
+
+namespace TSwim.Core.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ TSwim.Core/Models/Companies/Enums/InventoryControlMethod.cs
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Community, a coalition of the Good-Hearted Engineers
+// -------------------------------------------------------------------------------
+
+namespace TSwim.Core.Models.Companies.Enums
+{
+    public enum InventoryControlMethod
+    {
+        FIFO,
+        LIFO,
+        AVERAGE
+    }
+}
