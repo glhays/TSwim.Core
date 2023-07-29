79608790c2700d97493af370a7290c3a15b5e130
Greg Hays <glhays.dev@gmail.com>
2023-07-28 18:07:41 -0700

    CODE RUB: Startup Fix Renaming Api

--- TSwim.Core/Startup.cs
+++ TSwim.Core/Startup.cs
@@ -34,7 +34,7 @@ namespace TSwim.Core
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TSwim.Core.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TSwim.Core", Version = "v1" });
             });
         }
 
@@ -44,7 +44,7 @@ namespace TSwim.Core
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TSwim.Core.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TSwim.Core v1"));
             }
 
             app.UseHttpsRedirection();
